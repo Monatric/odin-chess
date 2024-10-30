2724531a167e7b5aee896636011a1bba7af17b3a
Monatric <nawpnawp8@gmail.com>
2024-10-31 01:53:45 +0800

    Refactor: change @board to @chessboard in Game

--- lib/game.rb
+++ lib/game.rb
@@ -1,12 +1,12 @@
 # class for Game that facilitates the chess game
 class Game
-  attr_reader :board, :player_white, :player_black, :current_turn
+  attr_reader :chessboard, :player_white, :player_black, :current_turn
 
-  def initialize(board,
+  def initialize(chessboard,
                  player_white = Player.new('Magnus', :white),
                  player_black = Player.new('Hikaru', :black),
                  current_turn = player_white)
-    @board = board
+    @chessboard = chessboard
     @player_white = player_white
     @player_black = player_black
     @current_turn = current_turn
