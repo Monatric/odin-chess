03e607d3abd7665581e2612ceea86fba8f0e51b5
Monatric <nawpnawp8@gmail.com>
2024-11-02 01:57:31 +0800

    Test: test out various behaviors in main.rb

--- lib/main.rb
+++ lib/main.rb
@@ -2,7 +2,19 @@ require_relative 'chessboard'
 require_relative 'pieces/pawn'
 require_relative 'pieces/piece'
 require_relative 'player'
+require_relative 'game'
+require_relative 'displayable'
 
-b = Chessboard.new
-p b.assemble(b.board)
-p b.board[:a1][:piece].symbol
+# b = Chessboard.new
+# magnus = Player.new('Magnus', :white)
+# hikaru = Player.new('Hikaru', :black)
+g = Game.new(Chessboard.new)
+board = g.chessboard
+board.assemble(g.player_white, g.player_black)
+board.show
+
+print 'Your move: '
+player_move = gets.chomp
+p player_move
+p g.valid_move?(player_move)
+board.show
