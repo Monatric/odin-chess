2d235dda9a54d68b0301be7bf1cd85789cfb7d86
Monatric <nawpnawp8@gmail.com>
2024-11-08 03:49:00 +0800

    Refactor: divide first_field to smaller chunks

--- lib/fen.rb
+++ lib/fen.rb
@@ -10,29 +10,41 @@ class FEN
     file = 'a'
     rank = 8
     space = 0
+    add_first_field_data(file, space, rank, chessboard)
+  end
+
+  private
+
+  def add_first_field_data(file, space, rank, chessboard)
     until rank.zero?
-      coordinate = (file + rank.to_s).to_sym
-      piece = chessboard.find_piece_by_coordinate(coordinate)
-      space += 1 if piece.nil?
-      fen_string << space.to_s unless piece.nil? || space.zero?
-
-      unless piece.nil?
-        fen_string << piece.notation
-        space = 0
-      end
-
-      file = (file.ord + 1).chr
-
-      next unless file == 'i'
-
-      fen_string << space.to_s unless space.zero?
-      fen_string << '/'
-      file = 'a'
-      space = 0
+      add_rank_data(file, space, rank, chessboard)
       rank -= 1
     end
   end
 
-  def move_row_first_field_generator(file, space, rank)
+  def add_rank_data(file, space, rank, chessboard)
+    until file == 'i'
+      coordinate = (file + rank.to_s).to_sym
+      piece = chessboard.find_piece_by_coordinate(coordinate)
+
+      space = update_fen_string(piece, space)
+      file = next_file(file)
+    end
+    fen_string << space.to_s unless space.zero?
+    fen_string << '/'
+  end
+
+  def next_file(file)
+    (file.ord + 1).chr
+  end
+
+  def update_fen_string(piece, space)
+    if piece.nil?
+      space + 1
+    else
+      fen_string << space.to_s unless space.zero?
+      fen_string << piece.notation
+      0
+    end
   end
 end
