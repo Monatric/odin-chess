7ca65baa32470e558f980fb969349e9ba10cf6b0
Monatric <nawpnawp8@gmail.com>
2024-11-11 00:40:44 +0800

    Refactor: blueprint methods for knight from pawns

--- lib/pieces/knight.rb
+++ lib/pieces/knight.rb
@@ -9,4 +9,19 @@ class Knight < Piece
   def symbol
     @color == :white ? '♘' : '♞'
   end
+
+  def can_move_to?(dest, chessboard)
+    calculate_possible_moves(chessboard).include?(dest)
+  end
+
+  private
+
+  def calculate_possible_moves(chessboard)
+    possible_moves = []
+    file = chessboard.current_coordinate(self)[0]
+    rank = chessboard.current_coordinate(self)[1].to_i
+    add_moves(file, rank, possible_moves, chessboard)
+
+    possible_moves
+  end
 end
