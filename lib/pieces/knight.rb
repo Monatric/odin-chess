280acdfd974ca1009baf83446b7d499cd931dd13
Monatric <nawpnawp8@gmail.com>
2024-11-12 08:01:56 +0800

    Feat: make movement logic for knight

--- lib/pieces/knight.rb
+++ lib/pieces/knight.rb
@@ -2,12 +2,29 @@ require_relative 'piece'
 
 # class for the knight
 class Knight < Piece
+  KNIGHT_OPTIONS = [
+    [2, 1],
+    [2, -1],
+    [1, 2],
+    [1, -2],
+    [-2, 1],
+    [-2, -1],
+    [-1, 2],
+    [-1, -2]
+  ]
+
   def notation
     @color == :white ? 'N' : 'n'
   end
 
   def symbol
     @color == :white ? '♘' : '♞'
+  end
+
+  def move(dest, chessboard)
+    source = chessboard.current_coordinate(self)
+    chessboard.remove_piece(source)
+    chessboard.add_piece(dest, self)
   end
 
   def can_move_to?(dest, chessboard)
@@ -18,10 +35,28 @@ class Knight < Piece
 
   def calculate_possible_moves(chessboard)
     possible_moves = []
-    file = chessboard.current_coordinate(self)[0]
-    rank = chessboard.current_coordinate(self)[1].to_i
+    position = chessboard.current_position(self)
+    return possible_moves if chessboard.find_coordinate_by_position(position).nil?
+
+    file = position[0]
+    rank = position[1]
     add_moves(file, rank, possible_moves, chessboard)
 
     possible_moves
+  end
+
+  def add_moves(file, rank, possible_moves, chessboard)
+    KNIGHT_OPTIONS.each do |option|
+      position = option[0] + file, option[1] + rank
+      coordinate = chessboard.find_coordinate_by_position(position)
+      next if coordinate.nil?
+
+      if chessboard.find_piece_by_coordinate(coordinate).nil?
+        possible_moves << coordinate
+      elsif chessboard.find_piece_by_coordinate(coordinate).color != color
+        possible_moves << coordinate
+      end
+    end
+    # later on, consider about checks/pins
   end
 end
