a4e1976f703c33a4010815f50394802e774938b3
Monatric <nawpnawp8@gmail.com>
2024-11-18 00:24:47 +0800

    Feat: add queen movement

--- lib/pieces/queen.rb
+++ lib/pieces/queen.rb
@@ -2,7 +2,7 @@ require_relative 'piece'
 
 # class for the Bishop
 class Queen < Piece
-  BISHOP_OPTIONS = [
+  QUEEN_OPTIONS = [
     # diagonal  
   [-1, 1],
     [1, 1],
@@ -27,5 +27,44 @@ class Queen < Piece
     calculate_possible_moves(chessboard).include?(dest)
   end
 
- 
+  private
+
+  def calculate_possible_moves(chessboard)
+    possible_moves = []
+    position = chessboard.current_position(self)
+    return possible_moves if chessboard.find_coordinate_by_position(position).nil? # possibly useless?
+
+    file = position[0]
+    rank = position[1]
+    add_moves(file, rank, possible_moves, chessboard)
+
+    possible_moves
+  end
+
+  def add_moves(file, rank, possible_moves, chessboard)
+    QUEEN_OPTIONS.each do |option|
+      queen_move = [file, rank]
+      calculate_queen_options(possible_moves, chessboard, queen_move, option)
+    end
+    # later on, consider about checks/pins
+  end
+
+  def calculate_queen_options(possible_moves, chessboard, queen_move, option)
+    loop do
+      queen_move = [queen_move[0] + option[0], queen_move[1] + option[1]]
+      coordinate = chessboard.find_coordinate_by_position(queen_move)
+      break if coordinate.nil?
+
+      if chessboard.find_piece_by_coordinate(coordinate).nil?
+        possible_moves << coordinate
+      elsif same_color_in_coordinate?(coordinate, chessboard)
+        break
+      elsif !same_color_in_coordinate?(coordinate, chessboard)
+        possible_moves << coordinate
+        break
+      end
+
+      # keep in mind the coordinate in possible_moves duplicates
+    end
+  end
 end
