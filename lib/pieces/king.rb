e5c2a4789e9e1dd18dda971326af9c615d2f24e1
Monatric <nawpnawp8@gmail.com>
2024-11-18 10:26:17 +0800

    Feat: add basic moves to king

--- lib/pieces/king.rb
+++ lib/pieces/king.rb
@@ -2,6 +2,17 @@ require_relative 'piece'
 
 # class for the Bishop
 class King < Piece
+  KING_OPTIONS = [
+    [0, 1], # forward
+    [1, 1], # upright
+    [1, 0], # right
+    [1, -1], # downright
+    [0, -1], # down
+    [-1, -1], # downleft
+    [-1, 0], # left
+    [-1, 1] # upleft
+  ]
+
   def notation
     @color == :white ? 'K' : 'k'
   end
@@ -28,30 +39,18 @@ class King < Piece
     possible_moves
   end
 
-  # def add_moves(file, rank, possible_moves, chessboard)
-  #   BISHOP_OPTIONS.each do |option|
-  #     bishop_move = [file, rank]
-  #     calculate_bishop_options(possible_moves, chessboard, bishop_move, option)
-  #   end
-  #   # later on, consider about checks/pins
-  # end
+  def add_moves(file, rank, possible_moves, chessboard)
+    KING_OPTIONS.each do |option|
+      position = option[0] + file, option[1] + rank
+      coordinate = chessboard.find_coordinate_by_position(position)
+      next if coordinate.nil?
 
-  # def calculate_bishop_options(possible_moves, chessboard, bishop_move, option)
-  #   loop do
-  #     bishop_move = [bishop_move[0] + option[0], bishop_move[1] + option[1]]
-  #     coordinate = chessboard.find_coordinate_by_position(bishop_move)
-  #     break if coordinate.nil?
-
-  #     if chessboard.find_piece_by_coordinate(coordinate).nil?
-  #       possible_moves << coordinate
-  #     elsif same_color_in_coordinate?(coordinate, chessboard)
-  #       break
-  #     elsif !same_color_in_coordinate?(coordinate, chessboard)
-  #       possible_moves << coordinate
-  #       break
-  #     end
-
-  #     # keep in mind the coordinate in possible_moves duplicates
-  #   end
-  # end
+      if chessboard.find_piece_by_coordinate(coordinate).nil?
+        possible_moves << coordinate
+      elsif chessboard.find_piece_by_coordinate(coordinate).color != color
+        possible_moves << coordinate
+      end
+    end
+    # later on, consider about checks/pins
+  end
 end
