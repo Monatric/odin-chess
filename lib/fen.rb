7289211cc2b2a1527242a1914a459dcf87cc1962
Monatric <nawpnawp8@gmail.com>
2024-12-30 03:54:50 +0800

    feat: add third field in fen

--- lib/fen.rb
+++ lib/fen.rb
@@ -7,6 +7,7 @@ class FEN
   def print_fen(chessboard, game)
     first_field(chessboard)
     second_field(game)
+    third_field(chessboard)
     fen_array_strings.join(' ')
   end
 
@@ -28,6 +29,34 @@ class FEN
   def second_field(game)
     current_color_turn = game.current_turn.color == :white ? 'w' : 'b'
     fen_array_strings.push(current_color_turn)
+  end
+
+  def third_field(chessboard)
+    field_string = []
+    add_white_castling_availability(chessboard, field_string)
+    add_black_castling_availability(chessboard, field_string)
+    result = field_string.join('')
+    fen_array_strings.push(result)
+  end
+
+  def add_white_castling_availability(chessboard, field_string)
+    h1_piece = chessboard.find_piece_by_coordinate(:h1)
+    a1_piece = chessboard.find_piece_by_coordinate(:a1)
+    e1_piece = chessboard.find_piece_by_coordinate(:e1)
+    field_string << 'K' if h1_piece.instance_of?(::Rook) && !h1_piece.moved
+    field_string << 'Q' if a1_piece.instance_of?(::Rook) && !a1_piece.moved
+    field_string.clear if e1_piece.instance_of?(::King) && e1_piece.moved
+    field_string
+  end
+
+  def add_black_castling_availability(chessboard, field_string)
+    h8_piece = chessboard.find_piece_by_coordinate(:h8)
+    a8_piece = chessboard.find_piece_by_coordinate(:a8)
+    e8_piece = chessboard.find_piece_by_coordinate(:e8)
+    field_string << 'k' if h8_piece.instance_of?(::Rook) && !h8_piece.moved
+    field_string << 'q' if a8_piece.instance_of?(::Rook) && !a8_piece.moved
+    field_string.clear if e8_piece.instance_of?(::King) && e8_piece.moved
+    field_string
   end
 
   def add_first_field_data(file, space, rank, chessboard, first_field_array)
