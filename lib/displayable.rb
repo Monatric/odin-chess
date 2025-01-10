7fd44ddcf1bf297bb6700cf22e23fa78481a2568
Monatric <nawpnawp8@gmail.com>
2025-01-10 11:49:19 +0800

    feat: enhance board lookup

--- lib/displayable.rb
+++ lib/displayable.rb
@@ -5,23 +5,26 @@ module Displayable
     rank = '8'
     loop_through_board_hash(board, file, rank)
 
-    puts "   +---+---+---+---+---+---+---+---+\n     a   b   c   d   e   f   g   h"
+    puts '     a   b   c   d   e   f   g   h'
   end
 
   private
 
   def loop_through_board_hash(board, file, rank)
+    puts '   ┌───┬───┬───┬───┬───┬───┬───┬───┐'
     until rank == '0'
-      puts '   +---+---+---+---+---+---+---+---+'
       print "#{rank}  "
       until file == 'i'
         piece = board[(file + rank).to_sym][:piece]
-        print "| #{piece.nil? ? ' ' : piece.symbol} "
+        print "│ #{piece.nil? ? ' ' : piece.symbol} "
         file = (file.ord + 1).chr
       end
-      puts '|'
+      puts '│'
       file = 'a'
       rank = (rank.to_i - 1).to_s
+      puts '   ├───┼───┼───┼───┼───┼───┼───┼───┤' unless rank == '0'
     end
+    puts '   └───┴───┴───┴───┴───┴───┴───┴───┘'
   end
 end
+# ┫ ┛ ┗ ┴ ┬ ┘ └ ┐ ┌ ─ ┼ ┻ ╋ ┣ ┏ ┓ ┤ ├ ┃
