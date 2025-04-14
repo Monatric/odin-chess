30e89007cb0afda425befa9ba7ec9751ccf966bd
Monatric <nawpnawp8@gmail.com>
2025-04-14 16:45:20 +0800

    feat: add convertable module and its coordinate converter

---
+++ lib/helpers/convertable.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+# methods to convert notations, symbols, and coordinates
+module Convertable
+  def coordinate_string_to_symbol(coordinate, file_offset: 0, rank_offset: 0)
+    file = coordinate[0]
+    rank = coordinate[1]
+
+    new_file = (file.to_s.ord + file_offset).chr
+    new_rank = (rank.to_s.ord + rank_offset).chr
+    p [new_rank, new_file]
+    (new_file + new_rank).to_sym
+  end
+end
