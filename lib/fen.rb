adf5d4436bb12840ccbe02e0966cc95b1f9704cb
Monatric <nawpnawp8@gmail.com>
2025-04-21 12:04:42 +0800

    refactor: rewrite HalfmoveClock into its own class

--- lib/fen.rb
+++ lib/fen.rb
@@ -2,11 +2,10 @@
 
 # class for generating, saving, loading, or getting/setting data for a valid chess position
 class FEN
-  def initialize(game = Game.new, chessboard = Chessboard.new, halfmove_clock = 0)
+  def initialize(game = Game.new, chessboard = Chessboard.new)
     @game = game
     @chessboard = chessboard
-    @halfmove_clock = halfmove_clock
-    @halfmove_clock_tracker = { piece_count: 0, pawn_coordinates: [], prev_piece_placement_field: '' }
+    @halfmove_clock = HalfmoveClockField.new(@chessboard)
   end
 
   def generate_fen
@@ -15,67 +14,11 @@ class FEN
     @fen_strings << ActiveColorField.generate(@game)
     @fen_strings << CastlingAvailabilityField.generate(@chessboard)
     @fen_strings << EnPassantField.generate(@chessboard)
-    @fen_strings << halfmove_clock_field
+    @fen_strings << @halfmove_clock.generate
     @fen_strings.join(' ')
   end
 
   private
 
   attr_accessor :fen_strings
-
-  def halfmove_clock_field
-    if @halfmove_clock_tracker[:prev_piece_placement_field] == ''
-      @halfmove_clock_tracker = temp_halfmove_clock_tracker
-      return @halfmove_clock
-    end
-
-    set_halfmove_clock
-
-    @halfmove_clock_tracker = temp_halfmove_clock_tracker
-    @halfmove_clock
-  end
-
-  def set_halfmove_clock
-    temp_piece_count = temp_halfmove_clock_tracker[:piece_count]
-    temp_pawn_coordinates = temp_halfmove_clock_tracker[:pawn_coordinates]
-    temp_piece_placement_field = temp_halfmove_clock_tracker[:prev_piece_placement_field]
-    if temp_piece_placement_field == @halfmove_clock_tracker[:prev_piece_placement_field]
-      @halfmove_clock
-    elsif halfmove_clock_tracker_empty? || same_halfmove_tracker?(temp_piece_count, temp_pawn_coordinates)
-      @halfmove_clock += 1
-    else
-      @halfmove_clock = 0
-    end
-  end
-
-  def same_halfmove_tracker?(temp_piece_count, temp_pawn_coordinates)
-    same_pawn_coordinates?(temp_pawn_coordinates) && same_piece_count?(temp_piece_count)
-  end
-
-  def same_pawn_coordinates?(temp_pawn_coordinates)
-    temp_pawn_coordinates == @halfmove_clock_tracker[:pawn_coordinates]
-  end
-
-  def same_piece_count?(temp_piece_count)
-    temp_piece_count == @halfmove_clock_tracker[:piece_count]
-  end
-
-  def halfmove_clock_tracker_empty?
-    @halfmove_clock_tracker[:piece_count].zero? && @halfmove_clock_tracker[:pawn_coordinates].empty?
-  end
-
-  def temp_halfmove_clock_tracker
-    temp_halfmove_clock_tracker = { piece_count: 0, pawn_coordinates: [], prev_piece_placement_field: '' }
-    ('a'..'h').each do |file|
-      8.downto(1).each do |rank|
-        coordinate = "#{file}#{rank}".to_sym
-        piece = @chessboard.find_piece_by_coordinate(coordinate)
-
-        temp_halfmove_clock_tracker[:piece_count] += 1 if piece
-        temp_halfmove_clock_tracker[:pawn_coordinates] << coordinate if piece.is_a? Pawn
-      end
-    end
-    temp_halfmove_clock_tracker[:prev_piece_placement_field] = PiecePlacementField.generate(@chessboard)
-    temp_halfmove_clock_tracker
-  end
 end
