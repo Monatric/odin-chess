140713d2f99926c3867b0ba81221c12a630b5a9f
Monatric <nawpnawp8@gmail.com>
2025-04-29 09:14:07 +0800

    feat: add spec knight and game files

--- spec/knight_spec.rb
+++
@@ -1,6 +0,0 @@
-require_relative '../lib/pieces/knight'
-require_relative '../lib/pieces/piece'
-
-require_relative '../lib/player'
-require_relative '../lib/game'
-require_relative '../lib/chessboard'
