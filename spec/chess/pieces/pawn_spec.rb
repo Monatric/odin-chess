77686b6eb185daa54154434e8a26bdec6eb2400f
Monatric <nawpnawp8@gmail.com>
2025-05-04 18:22:57 +0800

    test: pawn two steps empty, adjusted contexts

--- spec/chess/pieces/pawn_spec.rb
+++ spec/chess/pieces/pawn_spec.rb
@@ -12,18 +12,16 @@ require_relative '../../../lib/chess/chessboard'
 
 describe 'Pawn functionality' do
   describe '#can_move_to?' do
-    let(:fen_starting_position) { 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR/ w KQkq - 0 1' }
-    let(:chessboard) { Chess::Chessboard.new(fen_first_field: fen_starting_position) }
-    let(:game) { Chess::Game.new(chessboard) }
+    context 'when the white pawn is at e2' do
+      let(:fen_starting_position) { 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR/' }
+      let(:chessboard) { Chess::Chessboard.new(fen_first_field: fen_starting_position) }
+      let(:game) { Chess::Game.new(chessboard) }
 
-    context 'when the pieces are at the starting position' do
-      context 'when the white pawn is at e2' do
-        it 'can move two steps forward if the next two squares are empty' do
-          pawn = chessboard.find_piece_by_coordinate(:e2)
-          two_squares_forward = :e4
+      it 'can move two steps forward if the next two squares are empty' do
+        pawn = chessboard.find_piece_by_coordinate(:e2)
+        two_squares_forward = :e4
 
-          expect(pawn.can_move_to?(two_squares_forward, chessboard)).to be true
-        end
+        expect(pawn.can_move_to?(two_squares_forward, chessboard)).to be true
       end
     end
   end
