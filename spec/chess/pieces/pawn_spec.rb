a70c58dd12b8d2c53f883e1e1449670a1f1620b2
Monatric <nawpnawp8@gmail.com>
2025-05-15 02:17:07 +0800

    refactor: Pawn spec for less repeated code

--- spec/chess/pieces/pawn_spec.rb
+++ spec/chess/pieces/pawn_spec.rb
@@ -17,25 +17,20 @@ describe 'Pawn functionality' do
     context 'when the white pawn is at e2' do
       let(:fen_starting_position) { 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR/ w KQkq - 0 1' }
       let(:chessboard) { Chess::Chessboard.new(fen_string: fen_starting_position) }
+      let(:pawn) { chessboard.find_piece_by_coordinate(:e2) }
 
       it 'can move one step forward if the next square are empty' do
-        pawn = chessboard.find_piece_by_coordinate(:e2)
         one_square_forward = :e3
-
         expect(pawn.can_move_to?(one_square_forward, chessboard)).to be true
       end
 
       it 'can move two steps forward if the next two squares are empty' do
-        pawn = chessboard.find_piece_by_coordinate(:e2)
         two_squares_forward = :e4
-
         expect(pawn.can_move_to?(two_squares_forward, chessboard)).to be true
       end
 
       it 'cannot move more than two steps forward' do
-        pawn = chessboard.find_piece_by_coordinate(:e2)
         three_squares_forward = :e5
-
         expect(pawn.can_move_to?(three_squares_forward, chessboard)).to be false
       end
     end
