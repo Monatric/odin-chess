ac4dbab477780d3360491453ae8a560769a4b370
Monatric <nawpnawp8@gmail.com>
2025-05-15 02:22:16 +0800

    test: additional white pawn false movements

--- spec/chess/pieces/pawn_spec.rb
+++ spec/chess/pieces/pawn_spec.rb
@@ -34,5 +34,51 @@ describe 'Pawn functionality' do
         expect(pawn.can_move_to?(three_squares_forward, chessboard)).to be false
       end
     end
+
+    context 'when the white pawn is at e4, facing a black pawn at e5' do
+      let(:fen_two_opposing_pawns) { 'rnbqkbnr/pppp1ppp/8/4p3/4P3/8/PPPP1PPP/RNBQKBNR w KQkq - 0 2' }
+      let(:chessboard) { Chess::Chessboard.new(fen_string: fen_two_opposing_pawns) }
+      let(:pawn) { chessboard.find_piece_by_coordinate(:e4) }
+
+      it 'cannot move forward' do
+        one_square_forward = :e5
+        expect(pawn.can_move_to?(one_square_forward, chessboard)).to be false
+      end
+
+      it 'cannot move backward' do
+        one_square_backward = :e3
+        expect(pawn.can_move_to?(one_square_backward, chessboard)).to be false
+      end
+
+      it 'cannot move left' do
+        one_square_left = :d4
+        expect(pawn.can_move_to?(one_square_left, chessboard)).to be false
+      end
+
+      it 'cannot move right' do
+        one_square_right = :f4
+        expect(pawn.can_move_to?(one_square_right, chessboard)).to be false
+      end
+
+      it 'cannot move up left' do
+        one_square_up_left = :d5
+        expect(pawn.can_move_to?(one_square_up_left, chessboard)).to be false
+      end
+
+      it 'cannot move up right' do
+        one_square_up_right = :f5
+        expect(pawn.can_move_to?(one_square_up_right, chessboard)).to be false
+      end
+
+      it 'cannot move down left' do
+        one_square_down_left = :d3
+        expect(pawn.can_move_to?(one_square_down_left, chessboard)).to be false
+      end
+
+      it 'cannot move down right' do
+        one_square_down_right = :f3
+        expect(pawn.can_move_to?(one_square_down_right, chessboard)).to be false
+      end
+    end
   end
 end
