4519231436d879f78672d4b0464c89f6d28b4c5a
Monatric <nawpnawp8@gmail.com>
2025-05-25 03:31:55 +0800

    test: black knight movements

--- spec/chess/pieces/knight_spec.rb
+++ spec/chess/pieces/knight_spec.rb
@@ -45,5 +45,34 @@ describe 'Knight functionality' do
         end
       end
     end
+
+    context 'when the black knight is at the center with capturable white pieces' do
+      let(:fen) { '6k1/8/3P4/4R3/2n5/8/1B6/6K1 b - - 0 1' }
+      let(:chessboard) { Chess::Chessboard.new(fen_string: fen) }
+      let(:knight) { chessboard.find_piece_by_coordinate(:c4) }
+
+      legal_moves = {
+        a3: true, b2: true, d2: true, e3: true,
+        e5: true, d6: true, b6: true, a5: true
+      }
+
+      illegal_moves = {
+        b3: false, c3: false, d3: false,
+        b4: false, d4: false, b5: false,
+        c5: false, d5: false
+      }
+
+      legal_moves.each do |coordinate, allowed|
+        it "can move like an L shape (#{coordinate})" do
+          expect(knight.can_move_to?(coordinate, chessboard)).to be allowed
+        end
+      end
+
+      illegal_moves.each do |coordinate, disallowed|
+        it "cannot move to adjacent squares (#{coordinate})" do
+          expect(knight.can_move_to?(coordinate, chessboard)).to be disallowed
+        end
+      end
+    end
   end
 end
