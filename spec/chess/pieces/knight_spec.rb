95b4b634b3f5701bd9727ad6faddf1aeb2908fc9
Monatric <nawpnawp8@gmail.com>
2025-05-25 03:30:23 +0800

    refactor: change knight contexts

--- spec/chess/pieces/knight_spec.rb
+++ spec/chess/pieces/knight_spec.rb
@@ -17,33 +17,31 @@ require_relative '../../../lib/chess'
 
 describe 'Knight functionality' do
   describe '#can_move_to?' do
-    context 'white knight' do
-      context 'when the knight is at the center' do
-        let(:fen) { '6k1/8/3p4/4r3/2N5/8/1b6/6K1 w - - 0 1' }
-        let(:chessboard) { Chess::Chessboard.new(fen_string: fen) }
-        let(:knight) { chessboard.find_piece_by_coordinate(:c4) }
+    context 'when the white knight is at the center with capturable black pieces' do
+      let(:fen) { '6k1/8/3p4/4r3/2N5/8/1b6/6K1 w - - 0 1' }
+      let(:chessboard) { Chess::Chessboard.new(fen_string: fen) }
+      let(:knight) { chessboard.find_piece_by_coordinate(:c4) }
 
-        legal_moves = {
-          a3: true, b2: true, d2: true, e3: true,
-          e5: true, d6: true, b6: true, a5: true
-        }
+      legal_moves = {
+        a3: true, b2: true, d2: true, e3: true,
+        e5: true, d6: true, b6: true, a5: true
+      }
 
-        illegal_moves = {
-          b3: false, c3: false, d3: false,
-          b4: false, d4: false, b5: false,
-          c5: false, d5: false
-        }
+      illegal_moves = {
+        b3: false, c3: false, d3: false,
+        b4: false, d4: false, b5: false,
+        c5: false, d5: false
+      }
 
-        legal_moves.each do |coordinate, allowed|
-          it "can move like an L shape (#{coordinate})" do
-            expect(knight.can_move_to?(coordinate, chessboard)).to be allowed
-          end
+      legal_moves.each do |coordinate, allowed|
+        it "can move like an L shape (#{coordinate})" do
+          expect(knight.can_move_to?(coordinate, chessboard)).to be allowed
         end
+      end
 
-        illegal_moves.each do |coordinate, disallowed|
-          it "cannot move to adjacent squares (#{coordinate})" do
-            expect(knight.can_move_to?(coordinate, chessboard)).to be disallowed
-          end
+      illegal_moves.each do |coordinate, disallowed|
+        it "cannot move to adjacent squares (#{coordinate})" do
+          expect(knight.can_move_to?(coordinate, chessboard)).to be disallowed
         end
       end
     end
