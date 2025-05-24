a8cc9d609954301fdd20534e6a35af3237df1e37
Monatric <nawpnawp8@gmail.com>
2025-05-24 16:44:39 +0800

    refactor: contextualize white pawn in spec

--- spec/chess/pieces/pawn_spec.rb
+++ spec/chess/pieces/pawn_spec.rb
@@ -16,87 +16,89 @@ require_relative '../../../lib/chess'
 
 describe 'Pawn functionality' do
   describe '#can_move_to?' do
-    context 'when the pawn is white at starting position (e2)' do
-      let(:fen_starting_position) { 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR/ w KQkq - 0 1' }
-      let(:chessboard) { Chess::Chessboard.new(fen_string: fen_starting_position) }
-      let(:pawn) { chessboard.find_piece_by_coordinate(:e2) }
+    context 'when the pawn is white' do
+      context 'when the pawn is at starting position (e2)' do
+        let(:fen_starting_position) { 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR/ w KQkq - 0 1' }
+        let(:chessboard) { Chess::Chessboard.new(fen_string: fen_starting_position) }
+        let(:pawn) { chessboard.find_piece_by_coordinate(:e2) }
 
-      # expected outcome
-      moves = {
-        e3: true, # one square
-        e4: true, # two squares
-        e5: false # too far
-      }
+        # expected outcome
+        moves = {
+          e3: true, # one square
+          e4: true, # two squares
+          e5: false # too far
+        }
 
-      moves.each do |coordinate, allowed|
-        it "#{allowed ? 'allows' : 'disallows'} movement to #{coordinate}" do
-          expect(pawn.can_move_to?(coordinate, chessboard)).to be allowed
+        moves.each do |coordinate, allowed|
+          it "#{allowed ? 'allows' : 'disallows'} movement to #{coordinate}" do
+            expect(pawn.can_move_to?(coordinate, chessboard)).to be allowed
+          end
         end
       end
-    end
 
-    context 'when the pawn faces a black piece in front (at e4)' do
-      let(:fen_two_opposing_pawns) { 'rnbqkbnr/pppp1ppp/8/4p3/4P3/8/PPPP1PPP/RNBQKBNR w KQkq - 0 2' }
-      let(:chessboard) { Chess::Chessboard.new(fen_string: fen_two_opposing_pawns) }
-      let(:pawn) { chessboard.find_piece_by_coordinate(:e4) }
+      context 'when the pawn faces a black piece in front (at e4)' do
+        let(:fen_two_opposing_pawns) { 'rnbqkbnr/pppp1ppp/8/4p3/4P3/8/PPPP1PPP/RNBQKBNR w KQkq - 0 2' }
+        let(:chessboard) { Chess::Chessboard.new(fen_string: fen_two_opposing_pawns) }
+        let(:pawn) { chessboard.find_piece_by_coordinate(:e4) }
 
-      %i[e5 e3 d4 f4 d5 f5 d3 f3].each do |coordinate|
-        it "disallows movement to #{coordinate}" do
-          expect(pawn.can_move_to?(coordinate, chessboard)).to be false
+        %i[e5 e3 d4 f4 d5 f5 d3 f3].each do |coordinate|
+          it "disallows movement to #{coordinate}" do
+            expect(pawn.can_move_to?(coordinate, chessboard)).to be false
+          end
         end
       end
-    end
 
-    context 'when the pawn tries to capture (at e4, facing a black piece diagonally)' do
-      let(:fen_capturable_black_pieces) { 'rn1qkbnr/ppp1pppp/8/3p1b2/4P3/P7/1PPP1PPP/RNBQKBNR w KQkq - 1 3' }
-      let(:chessboard) { Chess::Chessboard.new(fen_string: fen_capturable_black_pieces) }
-      let(:pawn) { chessboard.find_piece_by_coordinate(:e4) }
+      context 'when the pawn tries to capture (at e4, facing a black piece diagonally)' do
+        let(:fen_capturable_black_pieces) { 'rn1qkbnr/ppp1pppp/8/3p1b2/4P3/P7/1PPP1PPP/RNBQKBNR w KQkq - 1 3' }
+        let(:chessboard) { Chess::Chessboard.new(fen_string: fen_capturable_black_pieces) }
+        let(:pawn) { chessboard.find_piece_by_coordinate(:e4) }
 
-      it 'can capture up left' do
-        one_square_up_left = :d5
-        result = pawn.can_move_to?(one_square_up_left, chessboard)
-        expect(result).to be true
+        it 'can capture up left' do
+          one_square_up_left = :d5
+          result = pawn.can_move_to?(one_square_up_left, chessboard)
+          expect(result).to be true
+        end
+
+        it 'can capture up right' do
+          one_square_up_right = :f5
+          result = pawn.can_move_to?(one_square_up_right, chessboard)
+          expect(result).to be true
+        end
       end
 
-      it 'can capture up right' do
-        one_square_up_right = :f5
-        result = pawn.can_move_to?(one_square_up_right, chessboard)
-        expect(result).to be true
+      context 'when the pawn tries to en passant to the left (at e5 with an en passantable black pawn on d5)' do
+        let(:fen_en_passantable_black_pawn) { 'rnbqkbnr/1pp1pppp/p7/3pP3/8/8/PPPP1PPP/RNBQKBNR w KQkq d6 0 3' }
+        let(:chessboard) { Chess::Chessboard.new(fen_string: fen_en_passantable_black_pawn) }
+        let(:pawn) { chessboard.find_piece_by_coordinate(:e5) }
+
+        it 'can capture black pawn on d5' do
+          en_passantable_square = :d6
+          result = pawn.can_move_to?(en_passantable_square, chessboard)
+          expect(result).to be true
+        end
       end
-    end
 
-    context 'when the pawn tries to en passant to the left (at e5 with an en passantable black pawn on d5)' do
-      let(:fen_en_passantable_black_pawn) { 'rnbqkbnr/1pp1pppp/p7/3pP3/8/8/PPPP1PPP/RNBQKBNR w KQkq d6 0 3' }
-      let(:chessboard) { Chess::Chessboard.new(fen_string: fen_en_passantable_black_pawn) }
-      let(:pawn) { chessboard.find_piece_by_coordinate(:e5) }
+      context 'when the pawn tries to en passant to the right (at a5 with an en passantable black pawn on b5)' do
+        let(:fen_en_passantable_black_pawn) { 'rnbqkbnr/p1ppppp1/7p/Pp6/8/8/1PPPPPPP/RNBQKBNR w KQkq b6 0 3' }
+        let(:chessboard) { Chess::Chessboard.new(fen_string: fen_en_passantable_black_pawn) }
+        let(:pawn) { chessboard.find_piece_by_coordinate(:a5) }
 
-      it 'can capture black pawn on d5' do
-        en_passantable_square = :d6
-        result = pawn.can_move_to?(en_passantable_square, chessboard)
-        expect(result).to be true
+        it 'can capture black pawn on b5' do
+          en_passantable_square = :b6
+          result = pawn.can_move_to?(en_passantable_square, chessboard)
+          expect(result).to be true
+        end
       end
-    end
 
-    context 'when the pawn tries to en passant to the right (at a5 with an en passantable black pawn on b5)' do
-      let(:fen_en_passantable_black_pawn) { 'rnbqkbnr/p1ppppp1/7p/Pp6/8/8/1PPPPPPP/RNBQKBNR w KQkq b6 0 3' }
-      let(:chessboard) { Chess::Chessboard.new(fen_string: fen_en_passantable_black_pawn) }
-      let(:pawn) { chessboard.find_piece_by_coordinate(:a5) }
+      context 'when the pawn checks the black king' do
+        let(:fen_checked_black_king) { '8/3k4/4P3/8/8/8/8/4K3 b - - 0 1' }
+        let(:chessboard) { Chess::Chessboard.new(fen_string: fen_checked_black_king) }
+        let(:pawn) { chessboard.find_piece_by_coordinate(:e6) }
 
-      it 'can capture black pawn on b5' do
-        en_passantable_square = :b6
-        result = pawn.can_move_to?(en_passantable_square, chessboard)
-        expect(result).to be true
-      end
-    end
-
-    context 'when the pawn checks the black king' do
-      let(:fen_checked_black_king) { '8/3k4/4P3/8/8/8/8/4K3 b - - 0 1' }
-      let(:chessboard) { Chess::Chessboard.new(fen_string: fen_checked_black_king) }
-      let(:pawn) { chessboard.find_piece_by_coordinate(:e6) }
-
-      it 'checks the black king' do
-        result = Chess::ThreatAnalyzer.in_check?(:black, chessboard)
-        expect(result).to be true
+        it 'checks the black king' do
+          result = Chess::ThreatAnalyzer.in_check?(:black, chessboard)
+          expect(result).to be true
+        end
       end
     end
 
