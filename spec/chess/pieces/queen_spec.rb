d3dd62bf2d2845b110e604fa7e00aa804791e461
Monatric <nawpnawp8@gmail.com>
2025-05-28 01:16:39 +0800

    test: queen movement

---
+++ spec/chess/pieces/queen_spec.rb
@@ -0,0 +1,49 @@
+# frozen_string_literal: true
+
+require_relative '../../../lib/chess/pieces/pawn'
+require_relative '../../../lib/chess/pieces/rook'
+require_relative '../../../lib/chess/pieces/knight'
+require_relative '../../../lib/chess/pieces/bishop'
+require_relative '../../../lib/chess/pieces/king'
+require_relative '../../../lib/chess/pieces/queen'
+require_relative '../../../lib/chess/piece'
+
+require_relative '../../../lib/player'
+require_relative '../../../lib/chess/chessboard/chessboard_assembler'
+require_relative '../../../lib/chess/chessboard'
+require_relative '../../../lib/helpers/threat_analyzer'
+require_relative '../../../lib/helpers/move_list'
+require_relative '../../../lib/chess'
+
+describe 'Queen functionality' do
+  describe '#can_move_to?' do
+    context 'when the white queen is at the center with capturable black pieces' do
+      let(:fen) { '5k2/8/4r3/8/2Q2p2/8/8/6K1 w - - 0 1' }
+      let(:chessboard) { Chess::Chessboard.new(fen_string: fen) }
+      let(:queen) { chessboard.find_piece_by_coordinate(:c4) }
+
+      illegal_moves = {
+        a3: false, b2: false, d2: false, e3: false,
+        e5: false, d6: false, b6: false, a5: false
+      }
+
+      legal_horizontal_moves = {
+        a4: true, b4: true, d4: true, e4: true, f4: true
+      }
+
+      illegal_moves.each do |coordinate, disallowed|
+        it "cannot move to adjacent squares (#{coordinate})" do
+          expect(queen.can_move_to?(coordinate, chessboard)).to be disallowed
+        end
+      end
+
+      context 'when the queen wants to move horizontally' do
+        legal_horizontal_moves.each do |coordinate, allowed|
+          it "can move horizontally (#{coordinate})" do
+            expect(queen.can_move_to?(coordinate, chessboard)).to be allowed
+          end
+        end
+      end
+    end
+  end
+end
