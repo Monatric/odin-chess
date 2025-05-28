796eaf5cda50a444fd0ce16703fc225768b97448
Monatric <nawpnawp8@gmail.com>
2025-05-28 22:22:54 +0800

    fix: test #update_fen and stub FEN initialization

--- spec/chess/game_spec.rb
+++ spec/chess/game_spec.rb
@@ -1,29 +1,34 @@
 # frozen_string_literal: true
 
-require_relative '../../lib/chess'
-
 describe Chess::Game do
   let(:player_one) { instance_double(Player) }
   let(:player_two) { instance_double(Player) }
-  let(:chessboard) { instance_double(Chessboard) }
+  let(:chessboard) { instance_double(Chess::Chessboard) }
+  let(:fen) { instance_double(Chess::FEN) }
+
   describe '#save_game' do
     # seems there's no need to test as it is well tested
   end
 
   describe '#update_fen' do
-    let(:fen) { instance_double(FEN) }
-    let(:new_fen_string) { 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1' }
-    subject(:game_update_fen) do
-      described_class.new(chessboard: chessboard,
-                          player_white: player_one,
-                          player_black: player_two,
-                          current_turn: player_one,
-                          fen: new_fen_string)
-    end
+    context 'when updating the string of FEN' do
+      let(:new_fen_string) { 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1' }
 
-    it 'sends generate_fen to fen' do
-      expect(fen).to receive(:generate_fen)
-      game_update_fen.update_fen
+      subject(:game_update_fen) do
+        described_class.new(chessboard: chessboard,
+                            player_white: player_one,
+                            player_black: player_two,
+                            current_turn: player_one,
+                            fen: new_fen_string)
+      end
+      before do
+        allow(Chess::FEN).to receive(:new).and_return(fen)
+      end
+
+      it 'sends generate_fen to fen' do
+        expect(fen).to receive(:generate_fen).and_return(new_fen_string)
+        game_update_fen.update_fen
+      end
     end
   end
 end
