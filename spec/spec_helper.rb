214fb36a48ddc507797d5a130388ac57deb997e3
Monatric <nawpnawp8@gmail.com>
2025-05-29 00:10:06 +0800

    feat: require move validator file in spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -35,6 +35,7 @@ require_relative '../lib/chess/chessboard/chessboard_assembler'
 require_relative '../lib/chess/chessboard'
 require_relative '../lib/helpers/threat_analyzer'
 require_relative '../lib/helpers/move_list'
+require_relative '../lib/helpers/move_validator'
 require_relative '../lib/chess'
 
 RSpec.configure do |config|
