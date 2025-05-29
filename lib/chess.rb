43c731df693a850862d2c719651a352cde7c9478
Monatric <nawpnawp8@gmail.com>
2025-05-30 00:34:08 +0800

    refactor: require all files under the Chess namespace

--- lib/chess.rb
+++ lib/chess.rb
@@ -5,10 +5,33 @@ module Chess
   require_relative 'chess/fen'
   require_relative 'chess/game'
   require_relative 'chess/chessboard'
-  Dir['chess/*.rb'].sort.each { |file| require_relative file }
-  Dir['chess/fen/*.rb'].sort.each { |file| require_relative file }
-  Dir['chess/pieces/*.rb'].sort.each { |file| require_relative file }
-  Dir['chess/pieces/advanced_piece_movement/*.rb'].sort.each { |file| require_relative file }
+  require_relative 'chess/piece'
+
+  require_relative 'chess/chessboard/chessboard_assembler'
+
+  require_relative 'chess/fen/active_color_field'
+  require_relative 'chess/fen/castling_availability_field'
+  require_relative 'chess/fen/en_passant_field'
+  require_relative 'chess/fen/fullmove_number_field'
+  require_relative 'chess/fen/halfmove_clock_field'
+  require_relative 'chess/fen/piece_placement_field'
+
+  require_relative 'chess/pieces/bishop'
+  require_relative 'chess/pieces/king'
+  require_relative 'chess/pieces/knight'
+  require_relative 'chess/pieces/pawn'
+  require_relative 'chess/pieces/queen'
+  require_relative 'chess/pieces/rook'
+  require_relative 'chess/pieces/advanced_piece_movement/king_castling'
+  require_relative 'chess/pieces/advanced_piece_movement/pawn_en_passant'
+  require_relative 'chess/pieces/advanced_piece_movement/pawn_promotion'
+
+  require_relative 'helpers/move_list'
+  require_relative 'helpers/move_validator'
+  require_relative 'helpers/threat_analyzer'
+
+  require_relative 'displayable'
+  require_relative 'player'
 
   RANK_ORDINALS = {
     first: 1,
