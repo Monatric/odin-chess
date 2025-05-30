ac73a04c0cb77dded631e43fd44ccc60da3aac35
Monatric <nawpnawp8@gmail.com>
2025-05-30 17:01:31 +0800

    test: Convertable module method for converting notation

---
+++ spec/helpers/convertable.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+describe Convertable do
+  let(:dummy_class) { Class.new { include Convertable } }
+  subject(:converter) { dummy_class.new }
+
+  describe '#notation_to_piece' do
+    context 'when a notation is passed for conversion' do
+      piece_notations = {
+        'r' => [Chess::Rook, :black],
+        'n' => [Chess::Knight, :black],
+        'b' => [Chess::Bishop, :black],
+        'q' => [Chess::Queen, :black],
+        'k' => [Chess::King, :black],
+        'p' => [Chess::Pawn, :black],
+        'R' => [Chess::Rook, :white],
+        'N' => [Chess::Knight, :white],
+        'B' => [Chess::Bishop, :white],
+        'Q' => [Chess::Queen, :white],
+        'K' => [Chess::King, :white],
+        'P' => [Chess::Pawn, :white]
+      }
+
+      piece_notations.each do |notation, expected_object|
+        expected_piece = expected_object[0]
+        expected_color = expected_object[1]
+        filtered_piece_name = expected_piece.to_s.split('::')[1]
+        it "returns a #{expected_color} #{filtered_piece_name} for #{notation}" do
+          piece = converter.notation_to_piece(notation)
+          expect(piece).to be_a(expected_piece)
+          expect(piece.color).to eq(expected_color)
+        end
+      end
+    end
+  end
+end
