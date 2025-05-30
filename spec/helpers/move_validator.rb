c93c764d43d826aec956c9db9b5f37e311c53b71
Monatric <nawpnawp8@gmail.com>
2025-05-30 23:23:37 +0800

    test: MoveValidator#valid_move? for when castling is invalid

---
+++ spec/helpers/move_validator.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+describe Chess::MoveValidator do
+  let(:game) { double('game') }
+  let(:chessboard) { double('chessboard') }
+  # subject(:move_validator) { described_class.new()}
+  describe '#valid_move?' do
+    context 'when the move attempt is castling but it is an illegal move' do
+      let(:castling_source) { :e1 }
+      let(:castling_dest) { :g1 }
+      subject(:move_validator) { described_class.new(source: castling_source, dest: castling_dest, game: game) }
+
+      before do
+        allow(game).to receive(:chessboard).and_return(chessboard)
+        allow(move_validator).to receive(:castling_attempt?).and_return(true)
+        allow(move_validator).to receive(:valid_castling?).and_return(false)
+      end
+
+      it 'returns false' do
+        result = move_validator.valid_move?
+        expect(result).to be false
+      end
+    end
+  end
+end
