35b33a8f4ae1e0475128d7bbf375962df6ac9712
Monatric <nawpnawp8@gmail.com>
2025-05-31 17:11:20 +0800

    docs: add notes to the remaining untested files

---
+++ spec/chess/fen_spec.rb
@@ -0,0 +1,4 @@
+# frozen_string_literal: true
+
+# NOTE: methods here are not something I have to test in CLI repeatedly. But it'd still probably
+# be nice to test this if I come back to it in the future, especially with the FEN helper classes.
---
+++ spec/chess_spec.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+# NOTE: methods here are not something I have to test in CLI repeatedly. And there's probably
+# nothing worthwhile to test here as it is simply a namespace and collection of constants and
+# imports
