78179ebc01fac4d7c1aade24980ad3c37f3e23c2
Monatric <nawpnawp8@gmail.com>
2025-05-31 16:48:02 +0800

    test: ThreatAnalyzer::in_check? for both cases

--- spec/helpers/threat_analyzer_spec.rb
+++ spec/helpers/threat_analyzer_spec.rb
@@ -6,7 +6,7 @@ describe Chess::ThreatAnalyzer do
   let(:game) { double('game') }
   subject(:threat_analyzer) { described_class }
 
-  describe '#checkmate' do
+  describe '::checkmate' do
     context 'when the legal squares of a player is empty' do
       before do
         allow(Chess::MoveList).to receive(:legal_squares_of_color).with(color, chessboard, game).and_return([])
@@ -25,6 +25,38 @@ describe Chess::ThreatAnalyzer do
 
       it 'returns false as the player still has legal moves' do
         expect(threat_analyzer.checkmate?(color, chessboard, game)).to be false
+      end
+    end
+  end
+
+  describe '::in_check?' do
+    let(:color) { :white }
+    let(:covered_squares) { %i[b4 c6 g8 e1 a2] }
+
+    context "when player's king is in check" do
+      let(:king_coordinate) { :e1 }
+
+      before do
+        allow(chessboard).to receive(:king_coordinate).with(color).and_return(king_coordinate)
+        allow(Chess::MoveList).to receive(:covered_squares_of_color).with(:black,
+                                                                          chessboard).and_return(covered_squares)
+      end
+
+      it "returns true as the king's coordinate is covered by the other player" do
+        expect(threat_analyzer.in_check?(color, chessboard)).to be true
+      end
+    end
+
+    context "when player's king is not in check" do
+      let(:king_coordinate) { :g1 }
+      before do
+        allow(chessboard).to receive(:king_coordinate).with(color).and_return(king_coordinate)
+        allow(Chess::MoveList).to receive(:covered_squares_of_color).with(:black,
+                                                                          chessboard).and_return(covered_squares)
+      end
+
+      it "returns false as the king's coordinate is not covered by the other player" do
+        expect(threat_analyzer.in_check?(color, chessboard)).to be false
       end
     end
   end
